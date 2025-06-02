6cd04ca79588e09d8463c3e959539f7dfbb5b0bb
damiandistefano <143121853+damiandistefano@users.noreply.github.com>
2025-06-01 21:17:54 -0300

    finished

--- TP4-Shell/src/ej1/ring.c
+++ TP4-Shell/src/ej1/ring.c
@@ -1,20 +1,93 @@
-#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#define ERROR_EXIT(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Uso: %s <n> <c> <s>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-int main(int argc, char **argv)
-{	
-	int start, status, pid, n;
-	int buffer[1];
+    int n = atoi(argv[1]);
+    int c = atoi(argv[2]);
+    int s = atoi(argv[3]);
 
-	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
-    
-    /* Parsing of arguments */
-  	/* TO COMPLETE */
-    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer[0], start);
-    
-   	/* You should start programming from here... */
+    if (n < 3 || s < 0 || s >= n) {
+        fprintf(stderr, "Error: n >= 3 y 0 <= s < n\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int pipes[n][2];
+    int padre_pipe[2];
+
+    for (int i = 0; i < n; ++i)
+        if (pipe(pipes[i]) == -1)
+            ERROR_EXIT("pipe");
+
+    if (pipe(padre_pipe) == -1)
+        ERROR_EXIT("pipe padre");
+
+    printf("Se crearán %d procesos, se enviará el caracter %d desde proceso %d\n", n, c, s);
+
+    for (int i = 0; i < n; ++i) {
+        pid_t pid = fork();
+        if (pid < 0)
+            ERROR_EXIT("fork");
+
+        if (pid == 0) {
+            // Cierro pipes que no uso
+            for (int j = 0; j < n; ++j) {
+                if (j != i) close(pipes[j][0]);
+                if (j != (i + 1) % n) close(pipes[j][1]);
+            }
+            close(padre_pipe[0]);
+
+            int num;
+
+            if (read(pipes[i][0], &num, sizeof(int)) != sizeof(int))
+                ERROR_EXIT("read hijo");
+
+            printf("Hijo %d recibe %d\n", i, num);
+
+            num++;
+
+            if ((i + 1) % n == s) {
+                if (write(padre_pipe[1], &num, sizeof(int)) != sizeof(int))
+                    ERROR_EXIT("write a padre");
+            } else {
+                if (write(pipes[(i + 1) % n][1], &num, sizeof(int)) != sizeof(int))
+                    ERROR_EXIT("write a siguiente");
+            }
+
+            close(pipes[i][0]);
+            close(pipes[(i + 1) % n][1]);
+            close(padre_pipe[1]);
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    // Padre
+    for (int i = 0; i < n; ++i) {
+        close(pipes[i][0]);
+        if (i != s) close(pipes[i][1]);
+    }
+    close(padre_pipe[1]);
+
+    if (write(pipes[s][1], &c, sizeof(int)) != sizeof(int))
+        ERROR_EXIT("write inicial padre");
+
+    int result;
+    if (read(padre_pipe[0], &result, sizeof(int)) != sizeof(int))
+        ERROR_EXIT("read padre");
+
+    printf("Resultado final: %d\n", result);
+    close(padre_pipe[0]);
+
+    for (int i = 0; i < n; ++i)
+        wait(NULL);
+
+    return 0;
 }
+
--- TP4-Shell/src/ej2/shell.c
+++ TP4-Shell/src/ej2/shell.c
@@ -6,42 +6,102 @@
 
 #define MAX_COMMANDS 200
 
-int main() {
+void parse_command(char *input, char **args) {
+    int i = 0;
+    char *ptr = input;
+    while (*ptr) {
+        while (*ptr == ' ') ptr++;
+        if (*ptr == '"') {
+            ptr++;
+            args[i++] = ptr;
+            while (*ptr && *ptr != '"') ptr++;
+            if (*ptr) {
+                *ptr = '\0';
+                ptr++;
+            }
+        } else {
+            args[i++] = ptr;
+            while (*ptr && *ptr != ' ') ptr++;
+            if (*ptr) {
+                *ptr = '\0';
+                ptr++;
+            }
+        }
+    }
+    args[i] = NULL;
+}
 
+char *trim(char *str) {
+    while (*str == ' ') str++;
+    char *end = str + strlen(str) - 1;
+    while (end > str && (*end == ' ' || *end == '\t')) {
+        *end = '\0';
+        end--;
+    }
+    return str;
+}
+
+int main() {
     char command[256];
     char *commands[MAX_COMMANDS];
     int command_count = 0;
 
-    while (1) 
-    {
+    while (1) {
         printf("Shell> ");
-        
-        /*Reads a line of input from the user from the standard input (stdin) and stores it in the variable command */
         fgets(command, sizeof(command), stdin);
-        
-        /* Removes the newline character (\n) from the end of the string stored in command, if present. 
-           This is done by replacing the newline character with the null character ('\0').
-           The strcspn() function returns the length of the initial segment of command that consists of 
-           characters not in the string specified in the second argument ("\n" in this case). */
         command[strcspn(command, "\n")] = '\0';
 
-        /* Tokenizes the command string using the pipe character (|) as a delimiter using the strtok() function. 
-           Each resulting token is stored in the commands[] array. 
-           The strtok() function breaks the command string into tokens (substrings) separated by the pipe character |. 
-           In each iteration of the while loop, strtok() returns the next token found in command. 
-           The tokens are stored in the commands[] array, and command_count is incremented to keep track of the number of tokens found. */
         char *token = strtok(command, "|");
-        while (token != NULL) 
-        {
-            commands[command_count++] = token;
+        while (token != NULL) {
+            commands[command_count++] = trim(token);
             token = strtok(NULL, "|");
         }
 
-        /* You should start programming from here... */
-        for (int i = 0; i < command_count; i++) 
-        {
-            printf("Command %d: %s\n", i, commands[i]);
-        }    
+        int pipefd[2];
+        int in_fd = 0;
+        pid_t pid;
+
+        for (int i = 0; i < command_count; i++) {
+            if (i < command_count - 1)
+                pipe(pipefd);
+
+            pid = fork();
+            if (pid == 0) {
+                if (in_fd != 0) {
+                    dup2(in_fd, 0);
+                    close(in_fd);
+                }
+                if (i < command_count - 1) {
+                    dup2(pipefd[1], 1);
+                    close(pipefd[0]);
+                    close(pipefd[1]);
+                }
+
+                char *args[64];
+                parse_command(commands[i], args);
+                execvp(args[0], args);
+                perror("execvp failed");
+                exit(1);
+            } else if (pid < 0) {
+                perror("fork failed");
+                exit(1);
+            } else {
+                if (in_fd != 0)
+                    close(in_fd);
+                if (i < command_count - 1) {
+                    close(pipefd[1]);
+                    in_fd = pipefd[0];
+                }
+            }
+        }
+
+        for (int i = 0; i < command_count; i++) {
+            wait(NULL);
+        }
+
+        command_count = 0;
     }
+
     return 0;
 }
+
